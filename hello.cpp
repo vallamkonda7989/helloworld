e01b6528e964acb28c9ee0b67e85ae9c70c380bb
vallamkonda7989 <71494033+vallamkonda7989@users.noreply.github.com>
2020-09-18 15:26:21 +0530

    printing the variable

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 using namespace std;
 int main()
-{
+{    int a=5;
     cout<<"welcome to git hub world";
     cout<<"welcome shiva to github world";
+    cout<<a;
     return 0;
     }
